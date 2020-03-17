9a84813f4f3969d4aa83bff2664623e85ac4a252
CrisRuedaP <crisruedap@gmail.com>
2020-03-17 21:33:04 +0000

    fixed betty

--- holberton.h
+++ holberton.h
@@ -3,28 +3,28 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h> 
+#include <unistd.h>
 
 
 /**
- * struct data_type - struct
+ * struct type - struct
  *
- * @type: format specifier 
+ * @type: format specifier
  * @f: function associated
  */
 typedef struct type
 {
 
-  char *type;
-  int (*f)(va_list args);
-} dt_t; 
+char *type;
+int (*f)(va_list args);
 
-int d_type_c(va_list args);  
+} dt_t;
+
+int d_type_c(va_list args);
 int d_type_s(va_list args);
 int d_type_p(va_list args);
 int d_type_d(va_list args);
 int d_type_i(va_list args);
 int _printf(const char *format, ...);
 int _putchar(char c);
-/*int _print_int(long n);*/
 #endif /* project our own prinf */
