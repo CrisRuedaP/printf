cd52c5cb2f4e6079713c811461ef6fc7655bf151
CrisRuedaP <crisruedap@gmail.com>
2020-03-18 05:21:22 +0000

    fixed betty

--- _printf.c
+++ _printf.c
@@ -5,52 +5,48 @@
 /**
  * _printf - function that produces output according to a format.
  * @format: a character string
- *
- * return: always 0
+ * Return: always 0
  */
 int _printf(const char *format, ...)
 {
-  int i = 0, j, count = 0;
-  va_list args;
-  dt_t data_t[] = {
-    {"c", d_type_c},
-    {"s", d_type_s},
-    {"%", d_type_p},
-    {"d", d_type_i},
-    {"i", d_type_i},
-    {NULL, NULL},
-  };
+int i = 0, j, count = 0;
+va_list args;
+dt_t data_t[] = {
+{"c", d_type_c}, {"s", d_type_s},
+{"%", d_type_p}, {"d", d_type_i},
+{"i", d_type_i}, {NULL, NULL}
+};
 
-  va_start(args, format);
-  if (format == NULL)
-    {
-      return (-1);
-    }
+va_start(args, format);
+if (format == NULL)
+{
+return (-1);
+}
 
-  while (format != '\0' && format[i] != '\0')
-    {
-      if (format[i] == '%')
-	{
-	  i++;
-	  if (format[i] == '\0')
-	    {
-	      return (-1);
-	    }
-	  j = 0;
-	  while (data_t[j].type != NULL)
-	    {
-	      if (format[i] == *(data_t[j].type))
-		count += data_t[j].f(args);
-	      j++;
-	    }
-	  i++;
-	}
-      if (format[i] != '%' && format[i] != '\0')
-	{
-	  count += _putchar(format[i]);
-	  i++;
-	}
-    }
-  va_end(args);
-  return (count);
+while (format != '\0' && format[i] != '\0')
+{
+if (format[i] == '%')
+{
+i++;
+if (format[i] == '\0')
+{
+return (-1);
+}
+j = 0;
+while (data_t[j].type != NULL)
+{
+if (format[i] == *(data_t[j].type))
+count += data_t[j].f(args);
+j++;
+}
+i++;
+}
+if (format[i] != '%' && format[i] != '\0')
+{
+count += _putchar(format[i]);
+i++;
+}
+}
+va_end(args);
+return (count);
 }
