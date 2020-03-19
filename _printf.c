ecfcce5b3a6f9549ed757af074db7d7eb14e7255
victorZ94 <zrvictor@outlook.com>
2020-03-19 22:22:59 +0000

    printf function formatt

--- _printf.c
+++ _printf.c
@@ -7,48 +7,48 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int i = 0, j, count = 0;
+va_list args;
+int i = 0, j, count = 0;
 
-	dt_t data_t[] = {
-	{"c", d_type_c}, {"s", d_type_s},
-	{"%", d_type_p}, {"d", d_type_i},
-	{"i", d_type_i}, {NULL, NULL},
-	};
+dt_t data_t[] = {
+{"c", d_type_c}, {"s", d_type_s},
+{"%", d_type_p}, {"d", d_type_i},
+{"i", d_type_i}, {NULL, NULL},
+};
 
-	va_start(args, format);
-	if (format == NULL)
-	{
-		return (-1);
-	}
+va_start(args, format);
+if (format == NULL)
+{
+return (-1);
+}
 
-	while (format != '\0' && format[i] != '\0')
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == '\0')
-			{
-				return (-1);
-			}
-			j = 0;
-			while (data_t[j].type != NULL)
-			{
-				if (format[i] == *(data_t[j].type))
-				{
-				count += data_t[j].f(args);
-				j++;
-				}
-			}
-			i++;
-		}
+while (format != '\0' && format[i] != '\0')
+{
+if (format[i] == '%')
+{
+i++;
+if (format[i] == '\0')
+{
+return (-1);
+}
+j = 0;
+while (data_t[j].type != NULL)
+{
+if (format[i] == *(data_t[j].type))
+{
+count += data_t[j].f(args);
+j++;
+}
+}
+i++;
+}
 
-		if (format[i] != '%' && format[i] != '\0')
-		{
-			count += _putchar(format[i]);
-			i++;
-		}
-	}
+if (format[i] != '%' && format[i] != '\0')
+{
+count += _putchar(format[i]);
+i++;
+}
+}
 
 va_end(args);
 return (count);
